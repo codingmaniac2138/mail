6575c06e6266131b08f5c0de9d0864a63d9e2eed
suhas.jadhav <suhas.j@consultadd.com>
2016-12-23 06:08:42 +0530

    wsgi changed

--- getmail/settings.py
+++ getmail/settings.py
@@ -119,6 +119,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+
 # # Static files (CSS, JavaScript, Images)
 # # https://docs.djangoproject.com/en/1.8/howto/static-files/
 # STATIC_ROOT = 'kdmail/static/'
